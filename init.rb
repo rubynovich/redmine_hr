ca2b48225d9e86206aba5d407cf4da52e0bc0540
Rubynovich <roman@shipiev.me>
2012-05-29 13:51:45 +0400

    add menu

--- init.rb
+++ init.rb
@@ -1,10 +1,29 @@
 require 'redmine'
+require 'dispatcher'
+require 'user_patch'
+
+Dispatcher.to_prepare do
+  User.send(:include, HR) unless User.include? HR
+end
 
 Redmine::Plugin.register :redmine_hr do
-  name 'Redmine Hr plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  name 'Redmine HR plugin'
+  author 'Roman Shipiev'
+  description 'Redmine plugin for HR-managers'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'http://github.com/rubynovich/redmine_hr'
+  author_url 'http://roman.shipiev.me'
+
+  menu :application_menu, :hr_statuses, 
+    {:controller => :hr_statuses, :action => :index}, 
+    :caption => :label_hr_statuses, 
+    :if => Proc.new{ User.is_hr? }    
+  menu :application_menu, :hr_jobs, 
+    {:controller => :hr_jobs, :action => :index}, 
+    :caption => :label_hr_jobs, 
+    :if => Proc.new{ User.is_hr? }
+  menu :application_menu, :hr_candidates, 
+    {:controller => :hr_candidates, :action => :index}, 
+    :caption => :label_hr_candidates, 
+    :if => Proc.new{ User.is_hr? }
 end
