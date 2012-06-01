046fc60efc2d9777e5900d6122be87febe4cf134
Rubynovich <roman@shipiev.me>
2012-06-01 09:47:22 +0400

    add en.yml

--- db/migrate/20120530052149_create_hr_statuses.rb
+++ db/migrate/20120530052149_create_hr_statuses.rb
@@ -1,7 +1,7 @@
 class CreateHrStatuses < ActiveRecord::Migration
   def self.up
     create_table :hr_statuses do |t|
-      t.column :name, :string, :default => "", :limit => 30
+      t.column :name, :string, :default => ""
       t.column :is_closed, :boolean, :default => false, :null => false
       t.column :is_default, :boolean, :default => false, :null => false
       t.column :position, :integer, :default => 1
