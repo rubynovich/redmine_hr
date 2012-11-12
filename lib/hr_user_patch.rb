66026654ea49b0158919b6091283692c972b1091
Rubynovich <roman@shipiev.me>
2012-11-12 17:17:18 +0400

    fix забыл удалить лишнюю }

--- lib/hr_user_patch.rb
+++ lib/hr_user_patch.rb
@@ -10,8 +10,7 @@ module HRPlugin
       
       base.class_eval do
         if Rails::VERSION::MAJOR >= 3 
-          scope :not_hr, where("#{User.table_name}.id NOT IN (SELECT hr_members.user_id FROM hr_members)")
-          }    
+          scope :not_hr, where("#{User.table_name}.id NOT IN (SELECT hr_members.user_id FROM hr_members)")    
         else
           named_scope :not_hr, lambda {
             { :conditions => ["#{User.table_name}.id NOT IN (SELECT hr_members.user_id FROM hr_members)"] }
