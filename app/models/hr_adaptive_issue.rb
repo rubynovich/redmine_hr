2957e8939ea8328bf3d93f6ffe84d4cdb04ee2ff
rubynovich <roman@shipiev.me>
2013-03-06 11:58:00 +0400

    add: hr_candidate#name to hr_adaptive_issue#subject

--- app/models/hr_adaptive_issue.rb
+++ app/models/hr_adaptive_issue.rb
@@ -1,21 +1,21 @@
 class HrAdaptiveIssue < ActiveRecord::Base
   unloadable
-  
+
   belongs_to :project
   belongs_to :tracker
-  belongs_to :hr_status  
+  belongs_to :hr_status
   belongs_to :assigned_to, :class_name => 'Principal', :foreign_key => 'assigned_to_id'
   belongs_to :priority, :class_name => 'IssuePriority', :foreign_key => 'priority_id'
-  
-  validates_presence_of :project_id, :tracker_id, :assigned_to_id, 
+
+  validates_presence_of :project_id, :tracker_id, :assigned_to_id,
     :priority_id, :subject, :start_date, :due_date, :hr_status_id
-  
+
   @@start_date_variants = ["now", "fwd"]
   @@due_date_variants   = ["1d_fwd", "fwd", "fwd_2w", "fwd_1m", "fwd_3m"]
-  
+
   cattr_accessor :due_date_variants, :start_date_variants
-  
-  if Rails::VERSION::MAJOR >= 3 
+
+  if Rails::VERSION::MAJOR >= 3
     scope :on_status, lambda{ |hr_status_id|
       if hr_status_id.present?
         where(:hr_status_id => hr_status_id)
@@ -26,23 +26,23 @@ class HrAdaptiveIssue < ActiveRecord::Base
       if hr_status_id.present?
         {:conditions => {:hr_status_id => hr_status_id}}
       end
-    }  
+    }
   end
-  
+
   def create_issue(hr_candidate)
     Issue.create(
-      :status => IssueStatus.default, 
-      :tracker => self.tracker, 
-      :subject => self.subject, 
-      :project => self.project, 
-      :description => [::I18n.t('message_about_hr_candidate', :name => hr_candidate.name, :job => hr_candidate.hr_job.name, :fwd => hr_candidate.due_date.strftime("%d.%m.%Y")), self.description].join("\n\n"), 
+      :status => IssueStatus.default,
+      :tracker => self.tracker,
+      :subject => [self.subject, hr_candidate.name].join(", "),
+      :project => self.project,
+      :description => [::I18n.t('message_about_hr_candidate', :name => hr_candidate.name, :job => hr_candidate.hr_job.name, :fwd => hr_candidate.due_date.strftime("%d.%m.%Y")), self.description].join("\n\n"),
       :author => User.current,
       :start_date => create_date(self.start_date, hr_candidate.due_date),
       :due_date => create_date(self.due_date, hr_candidate.due_date),
       :priority => self.priority,
       :assigned_to => self.assigned_to)
   end
-  
+
   def create_date(str, fwd)
     case str
       when "now"
@@ -59,15 +59,15 @@ class HrAdaptiveIssue < ActiveRecord::Base
         fwd + 2.months + 2.weeks
     end
   end
-  
+
   def to_s
     subject
   end
-  
+
   def to_hash
     self.class.column_names.
-      inject({}) { |result, name| 
+      inject({}) { |result, name|
         result.merge name => self.send(name)
       }
-  end  
+  end
 end
