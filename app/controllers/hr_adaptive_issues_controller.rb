6be8eb5d6618e9b3adc43f42465bf1d8f7dfd020
rubynovich <roman@shipiev.me>
2013-03-21 10:37:57 +0400

    fix: change order

--- app/controllers/hr_adaptive_issues_controller.rb
+++ app/controllers/hr_adaptive_issues_controller.rb
@@ -5,27 +5,27 @@ class HrAdaptiveIssuesController < ApplicationController
   before_filter :new_adaptive_issue, :only => [:new, :create]
 
   def index
-    @hr_adaptive_issues = HrAdaptiveIssue.all(:order => 'hr_status_id')
+    @hr_adaptive_issues = HrAdaptiveIssue.all(:order => 'hr_status_id, subject')
   end
-  
-  def new    
+
+  def new
   end
-  
+
   def edit
   end
-  
+
   def show
   end
-    
+
   def create
     if @hr_adaptive_issue.save
       flash[:notice] = l(:notice_successful_create)
       redirect_to :action => 'index'
     else
       render :action => :new
-    end      
+    end
   end
-  
+
   def update
     if @hr_adaptive_issue.update_attributes(params[:hr_adaptive_issue])
       flash[:notice] = l(:notice_successful_update)
@@ -34,25 +34,25 @@ class HrAdaptiveIssuesController < ApplicationController
       render :action => "edit"
     end
   end
-  
+
   def destroy
     @hr_adaptive_issue.destroy
     redirect_to :action => 'index'
   rescue
     flash[:error] = l(:error_unable_delete_hr_adaptive_issue)
     redirect_to :action => 'index'
-  end  
+  end
 
 
   private
     def new_adaptive_issue
-      @hr_adaptive_issue = HrAdaptiveIssue.new params[:hr_adaptive_issue]    
+      @hr_adaptive_issue = HrAdaptiveIssue.new params[:hr_adaptive_issue]
     end
-    
+
     def find_adaptive_issue
-      @hr_adaptive_issue = HrAdaptiveIssue.find(params[:id])    
+      @hr_adaptive_issue = HrAdaptiveIssue.find(params[:id])
     end
-  
+
     def require_hr
       (render_403; return false) unless User.current.is_hr?
     end
