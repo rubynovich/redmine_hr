1b1a69e014dfa8671f72c3ccc9057e9180ff672a
rubynovich <roman@shipiev.me>
2013-10-23 12:11:17 +0400

    fix: addaptive_issue.assigned_to

--- app/models/hr_adaptive_issue.rb
+++ app/models/hr_adaptive_issue.rb
@@ -7,7 +7,7 @@ class HrAdaptiveIssue < ActiveRecord::Base
   belongs_to :assigned_to, class_name: 'Principal', foreign_key: 'assigned_to_id'
   belongs_to :priority, class_name: 'IssuePriority', foreign_key: 'priority_id'
 
-  validates_presence_of :project_id, :tracker_id, :assigned_to_id,
+  validates_presence_of :project_id, :tracker_id,
     :priority_id, :subject, :start_date, :due_date, :hr_status_id
 
   @@start_date_variants = ["now", "fwd", "fwd_2w", "fwd_1m", "fwd_3m"]
@@ -35,12 +35,12 @@ class HrAdaptiveIssue < ActiveRecord::Base
       :tracker => self.tracker,
       :subject => [self.subject, hr_candidate.name].join(", "),
       :project => self.project,
-      :description => [::I18n.t('message_about_hr_candidate', :name => hr_candidate.name, :job => hr_candidate.hr_job.name, :fwd => hr_candidate.due_date.strftime("%d.%m.%Y")), self.description].join("\n\n"),
+      :description => [::I18n.t('message_about_hr_candidate', name: hr_candidate.name, job: hr_candidate.hr_job.name, fwd: hr_candidate.due_date.strftime("%d.%m.%Y")), self.description].join("\n\n"),
       :author => User.current,
       :start_date => create_date(self.start_date, hr_candidate.due_date),
       :due_date => create_date(self.due_date, hr_candidate.due_date),
       :priority => self.priority,
-      :assigned_to => self.assigned_to,
+      :assigned_to => (self.assigned_to || User.current),
       :estimated_hours => self.estimated_hours
       )
   end
